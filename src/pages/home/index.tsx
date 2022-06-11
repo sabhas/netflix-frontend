a689915297db6ae7924f21f163534e737fb1a468
Sabir Hassan <sabirbk06@gmail.com>
2022-06-11 11:17:23 +0500

    add lists

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,17 @@
 import "./home.scss"
 import Navbar from "../../components/navbar"
 import Featured from "../../components/featured"
+import List from "../../components/list"
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   )
 }
